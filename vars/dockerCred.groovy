99ac9ea7241800d75f33c3312ec6178c530be458
Naman Singh <141719809+namanSingh101@users.noreply.github.com>
2025-11-08 14:20:10 +0530

    Fix syntax for withCredentials in dockerCred.groovy

--- vars/dockerCred.groovy
+++ vars/dockerCred.groovy
@@ -1,6 +1,6 @@
 def call(String credentialsId){
   echo "Login into Docker Hub"
-  withCredentials([usernamePassword(credentialsId:credentialsId,usernameVariable:'DOCKERHUB_USR,passwordVariable:'DOCKERHUB_PSW')]){
+  withCredentials([usernamePassword(credentialsId:credentialsId,usernameVariable:'DOCKERHUB_USR',passwordVariable:'DOCKERHUB_PSW')]){
                 sh """
                      echo $DOCKERHUB_PSW | docker login -u $DOCKERHUB_USR --password-stdin
                    """
